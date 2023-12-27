657fcf3827ce81ac17dc56cdfde05c5d3c7c2ae0
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2023-12-27 12:58:50 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,7 @@
+it("should open teh website correctly",function(){
+console.log("test case 1 steps");
+});
+
+it("should login teh website correctly",function(){
+    console.log("test case 2 steps");
+    });
