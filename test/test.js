1d05ebfd9c36165079d08963ca82215c6a78bb0b
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2023-12-27 13:57:46 +0200

    describe

--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
-it("should open teh website correctly",function(){
-console.log("test case 1 steps");
-});
 
-it("should login teh website correctly",function(){
-    console.log("test case 2 steps");
-    });
+describe("login test cases",function(){
+    
+    it("should be able to login with valid email and password",function(){
+        console.log("open the website");
+        console.log("click on login menu button");
+        console.log("fill a correct emial address");
+        console.log("fill a correct password");
+        console.log("click on the login button");
+        console.log("make sure that the user was logged in correctly");
+        });
+        
+        
+        it("should not be able to login with valid email and invalid password",function(){
+            console.log("open the website");
+            console.log("click on login menu button");
+            console.log("fill a correct emial address");
+            console.log("fill invalid password");
+            console.log("click on the login button");
+            console.log("make sure that an error message will be displayed");
+            });
+})
+
+
