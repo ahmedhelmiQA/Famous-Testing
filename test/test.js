1c0bd435811313a8ddd8ff32fcb650ef91066b54
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-28 19:58:26 +0200

    before&after

--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
 
+before(function(){
+    console.log("Run the application locally");
+    console.log("Run the server");
+    console.log("open the website");
+    console.log("click on login menu button");
+    console.log("fill a correct emial address");
+})
+after(function(){
+    console.log("stop the application locally");
+    console.log("stop the server");
+})
 describe("login test cases",function(){
     
     it("should be able to login with valid email and password",function(){
-        console.log("open the website");
-        console.log("click on login menu button");
-        console.log("fill a correct emial address");
         console.log("fill a correct password");
         console.log("click on the login button");
         console.log("make sure that the user was logged in correctly");
@@ -12,9 +20,6 @@ describe("login test cases",function(){
         
         
         it("should not be able to login with valid email and invalid password",function(){
-            console.log("open the website");
-            console.log("click on login menu button");
-            console.log("fill a correct emial address");
             console.log("fill invalid password");
             console.log("click on the login button");
             console.log("make sure that an error message will be displayed");
