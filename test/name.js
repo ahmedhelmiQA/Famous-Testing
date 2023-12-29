d2eb72e2617eb8ecfb07f31053c458e0bd42c0ef
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-29 22:04:52 +0200

    assert

--- test/name.js
+++ test/name.js
@@ -1,8 +1,22 @@
-const checkName = require("..//name");
+// const checkName = require("..//name");
+
+// describe("check name test case",function(){
+//     it("should returns USER ifnthe name contains @",function(){
+//         let result = checkName("ahm@d");
+//         console.log(result);
+//     })
+// })
+
+
+      // assert
+const { assert } = require("chai");
+const checkName = require("../name");
+
 
 describe("check name test case",function(){
-    it("should returns USER ifnthe name contains @",function(){
-        let result = checkName("ahm@d");
-        console.log(result);
+    it.only("should returns USER if the name contains @",function(){
+        let expectedresult = "USER";
+        let actualresult = checkName("ahm@d");
+        assert.equal(actualresult,expectedresult);
     })
 })
