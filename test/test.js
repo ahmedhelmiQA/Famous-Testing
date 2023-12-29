d50b7e696eb79fdef4e649cf8e5558b812e8db6c
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-29 16:25:49 +0200

    it.only

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-
+///                            before  - after  - beforeEach  - afterEach
+/*
 before(function(){       // once time before run the all test  cases //
     console.log("Run the application locally"); 
     console.log("Run the server");
@@ -35,12 +36,37 @@ describe("login test cases",function(){
             console.log("make sure that an error message will be displayed");
             });
 
-    it("shoud not be able to login with invald email and vaild password",function(){
+    it.only("shoud not be able to login with invald email and vaild password",function(){
         console.log("fill invalid email address");
         console.log("fill a correct password");
         console.log("click on the login button");
         console.log("make sure that an error message will be displayed");
     })
+})
+*/
+
+
+   
+it("should be able to login with valid email and password",function(){
+    console.log("fill a correct emial address");
+    console.log("fill a correct password");
+    console.log("click on the login button");
+    console.log("make sure that the user was logged in correctly");
+    });
+    
+    
+    it("should not be able to login with valid email and invalid password",function(){
+        console.log("fill a correct emial address");
+        console.log("fill invalid password");
+        console.log("click on the login button");
+        console.log("make sure that an error message will be displayed");
+        });
+
+it.only("shoud not be able to login with invald email and vaild password",function(){
+    console.log("fill invalid email address");
+    console.log("fill a correct password");
+    console.log("click on the login button");
+    console.log("make sure that an error message will be displayed");
 })
 
 
