947e4df8a0e6cb95fa2be926c794253902cd35d4
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-29 16:40:29 +0200

    npm install chai

--- test/test.js
+++ test/test.js
@@ -45,28 +45,31 @@ describe("login test cases",function(){
 })
 */
 
-
+describe("login test cases",function(){
    
-it("should be able to login with valid email and password",function(){
-    console.log("fill a correct emial address");
-    console.log("fill a correct password");
-    console.log("click on the login button");
-    console.log("make sure that the user was logged in correctly");
-    });
-    
-    
-    it.skip("should not be able to login with valid email and invalid password",function(){
+    it("should be able to login with valid email and password",function(){
         console.log("fill a correct emial address");
-        console.log("fill invalid password");
+        console.log("fill a correct password");
+        console.log("click on the login button");
+        console.log("make sure that the user was logged in correctly");
+        });
+        
+        
+        it.skip("should not be able to login with valid email and invalid password",function(){
+            console.log("fill a correct emial address");
+            console.log("fill invalid password");
+            console.log("click on the login button");
+            console.log("make sure that an error message will be displayed");
+            });
+    
+    it("shoud not be able to login with invald email and vaild password",function(){
+        console.log("fill invalid email address");
+        console.log("fill a correct password");
         console.log("click on the login button");
         console.log("make sure that an error message will be displayed");
-        });
+    })
+    
 
-it("shoud not be able to login with invald email and vaild password",function(){
-    console.log("fill invalid email address");
-    console.log("fill a correct password");
-    console.log("click on the login button");
-    console.log("make sure that an error message will be displayed");
 })
 
 
