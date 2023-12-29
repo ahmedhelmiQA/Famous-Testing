190e2e7ea8f239292294fb8cf884a3c29fc262e4
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-29 16:29:18 +0200

    it.skip

--- test/test.js
+++ test/test.js
@@ -55,14 +55,14 @@ it("should be able to login with valid email and password",function(){
     });
     
     
-    it("should not be able to login with valid email and invalid password",function(){
+    it.skip("should not be able to login with valid email and invalid password",function(){
         console.log("fill a correct emial address");
         console.log("fill invalid password");
         console.log("click on the login button");
         console.log("make sure that an error message will be displayed");
         });
 
-it.only("shoud not be able to login with invald email and vaild password",function(){
+it("shoud not be able to login with invald email and vaild password",function(){
     console.log("fill invalid email address");
     console.log("fill a correct password");
     console.log("click on the login button");
