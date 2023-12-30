465f8477b90ce2639bdcd8e6362329f87acdd01e
ahmed helmi QA <ahmed.systemtester@gmail.com>
2023-12-30 16:12:04 +0200

    test case by assert

--- test/name.js
+++ test/name.js
@@ -20,3 +20,20 @@ describe("check name test case",function(){
         assert.equal(actualresult,expectedresult);
     })
 })
+  
+
+describe("check name test case",function(){
+    it.only("should returns USER if the name contains .",function(){
+        let expectedresult = "USER";
+        let actualresult = checkName("ahm.d");
+        assert.equal(actualresult,expectedresult);
+    })
+})
+
+describe("check name test case",function(){
+    it.only("should returns name ",function(){
+        let expectedresult = "ahmed";
+        let actualresult = checkName("ahmed");
+        assert.equal(actualresult,expectedresult);
+    })
+})
